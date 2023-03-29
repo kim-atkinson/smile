2c2af7e339eb62ecd4b117959fa215069f2ab3c9
kim-atkinson <kimfromtoronto@gmail.com>
2023-03-29 11:52:29 -0400

    Box shadow added to header on scroll.

---
+++ header.js
@@ -0,0 +1,31 @@
+// When user scrolls down the page the header will turn white. 
+// Add a class to make the header white
+
+const headerTag = document.querySelector("header")
+
+const toggleHeader = function () {
+    const pixels = window.pageYOffset
+
+    if (pixels > 64) {
+        headerTag.classList.add("scrolled")
+    } else {
+        headerTag.classList.remove("scrolled")
+    }
+}
+
+const fadeBox = function () {
+    const pixels = window.pageYOffset
+    const alpha = Math.min(pixels / 200, 0.25)
+
+
+    headerTag.style.boxShadow = `0 0 10px rgba(0, 0, 0, ${alpha})`
+}
+
+
+// toggleHeader()
+// fadeBox()
+
+document.addEventListener("scroll", function () {
+    toggleHeader()
+    fadeBox()
+})
