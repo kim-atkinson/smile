c7c71c6c339163fbd3e92fa41162a6c8f0ad64a0
kim-atkinson <kimfromtoronto@gmail.com>
2023-03-30 19:55:11 -0400

    Refined mousemovement to the irises in the logo.

--- js/eyes.js
+++ js/eyes.js
@@ -2,8 +2,34 @@ const irisLeft = document.querySelector("div.iris-left")
 const irisRight = document.querySelector("div.iris-right")
 
 const moveEye = function (tag, mouseX, mouseY) {
-    tag.style.left = mouseX + "px"
-    tag.style.top = mouseY + "px"
+    // tag.style.left = mouseX + "px"
+    // tag.style.top = mouseY + "px"
+
+    // Get the center of the eye
+    const eyeMidX = tag.getBoundingClientRect().left
+    const eyeMidY = tag.getBoundingClientRect().top
+
+    // Find the difference between the eye and the mouse
+    const diffX = mouseX - eyeMidX
+    const diffY = mouseY - eyeMidY
+
+    // Pythagoros therrm
+    const diff = Math.sqrt(diffX + diffX + diffY + diffY)
+
+    // What is the capped radius
+    const radius = Math.min(3, diff)
+
+    // tan in math
+    const angle = Math.atan2(diffY, diffX)
+
+    // Get the capped version based of the anhle
+    const cappedX = radius * Math.cos(angle)
+    const cappedY = radius * Math.sin(angle)
+
+    const eyeTag = tag.querySelector("div")
+
+    eyeTag.style.left = cappedX + "px"
+    eyeTag.style.top = cappedY + "px"
 }
 
 document.addEventListener("mousemove", function (event) {
